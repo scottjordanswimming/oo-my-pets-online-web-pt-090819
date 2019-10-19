29f77125572930b01852ffb0127c15a42a70facd
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-19 21:30:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,13 @@
 class Owner
   
-  attr_reader :name 
+  attr_reader :namem :species
   attr_accessor :species
   
   def initialize(name)
     @name = name
     @species = "human"
 end
+
 
 
 end
